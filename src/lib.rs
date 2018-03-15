ad10cf47982315e68009e6f745cedb1ecc187d28
Matthias Herzog <37505324+kegato@users.noreply.github.com>
2018-03-15 22:25:41 +0100

    inital commit

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+extern crate cookie;
+extern crate reqwest;
+extern crate sxd_document;
+extern crate sxd_xpath;
+
+mod rpc;
+pub mod inwx;
---
+++ src/rpc.rs
@@ -0,0 +1,159 @@
+use reqwest::{Client, Response, StatusCode};
+use reqwest::header::{Cookie, SetCookie};
+use sxd_document::writer::format_document;
+use sxd_document::{parser, Package};
+use sxd_document::dom::Document;
+use sxd_xpath::evaluate_xpath;
+use cookie;
+
+pub struct RpcRequestParameter {
+    pub name: &'static str,
+    pub value: RpcRequestParameterValue,
+}
+
+pub enum RpcRequestParameterValue {
+    String(String),
+    Int(i32),
+}
+
+pub struct RpcRequest {
+    body: Vec<u8>,
+    cookie: Option<Cookie>,
+    method: String,
+}
+
+impl RpcRequest {
+    pub fn new(method: &str, parameters: &[RpcRequestParameter]) -> RpcRequest {
+        let package = Package::new();
+        let doc = package.as_document();
+
+        let method_call = doc.create_element("methodCall");
+        doc.root().append_child(method_call);
+
+        let method_name = doc.create_element("methodName");
+        method_name.append_child(doc.create_text(method));
+        method_call.append_child(method_name);
+
+        let params = doc.create_element("params");
+        method_call.append_child(params);
+        let param = doc.create_element("param");
+        params.append_child(param);
+        let value = doc.create_element("value");
+        param.append_child(value);
+        let s = doc.create_element("struct");
+        value.append_child(s);
+
+        for param in parameters {
+            let member = doc.create_element("member");
+
+            let name = doc.create_element("name");
+            name.append_child(doc.create_text(&param.name));
+            member.append_child(name);
+
+            let value = doc.create_element("value");
+            member.append_child(value);
+            match param.value {
+                RpcRequestParameterValue::String(ref val) => {
+                    let string = doc.create_element("string");
+                    string.append_child(doc.create_text(val));
+                    value.append_child(string);
+                }
+                RpcRequestParameterValue::Int(ref val) => {
+                    let string = doc.create_element("int");
+                    string.append_child(doc.create_text(&val.to_string()));
+                    value.append_child(string);
+                }
+            }
+
+            s.append_child(member);
+        }
+
+        let mut body = Vec::new();
+        format_document(&doc, &mut body).unwrap();
+
+        RpcRequest {
+            body: body,
+            cookie: None,
+            method: method.to_owned(),
+        }
+    }
+
+    pub fn set_cookie(&mut self, cookie: Cookie) {
+        self.cookie = Some(cookie);
+    }
+
+    pub fn get_method(&self) -> String {
+        self.method.clone()
+    }
+
+    pub fn send(self, url: &str) -> Option<RpcResponse> {
+        let client = Client::new();
+
+        let mut request = client.post(url);
+        request.body(self.body);
+
+        if let Some(cookie) = self.cookie {
+            request.header(cookie);
+        }
+
+        match request.send() {
+            Ok(response) => RpcResponse::new(response),
+            Err(_) => None,
+        }
+    }
+}
+
+pub struct RpcResponse {
+    success: bool,
+    cookie: Cookie,
+    package: Package,
+}
+
+impl RpcResponse {
+    fn new(mut response: Response) -> Option<RpcResponse> {
+        if response.status() == StatusCode::Ok {
+            if let Ok(ref response_text) = response.text() {
+                if let Ok(package) = parser::parse(response_text) {
+                    let mut success = false;
+                    let mut cookie = Cookie::new();
+
+                    if let Some(&SetCookie(ref set_cookies)) = response.headers().get::<SetCookie>() {
+                        for set_cookie in set_cookies {
+                            if let Ok(c) = cookie::Cookie::parse(set_cookie.to_owned()) {
+                                cookie.append(c.name().to_owned(), c.value().to_owned());
+                            }
+                        }
+                    }
+
+                    if let Ok(code) = evaluate_xpath(&package.as_document(), "/methodResponse/params/param/value/struct/member[name/text()=\"code\"]/value/int/text()") {
+                        if let Ok(code) = code.string().parse::<i32>() {
+                            if code < 2000 {
+                                success = true;
+                            }
+                        }
+                    }
+
+                    return Some(RpcResponse {
+                        success,
+                        cookie,
+                        package,
+                    });
+                }
+            }
+        }
+
+        None
+    }
+
+    pub fn is_success(&self) -> bool {
+        self.success
+    }
+
+    pub fn get_cookie(&self) -> Cookie {
+        self.cookie.clone()
+    }
+
+    pub fn get_document(&self) -> Document {
+        self.package.as_document()
+    }
+}
